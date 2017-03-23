9f2db3ca830a7cff060d0016af3e2a30e9e9d5e8
chris <chrisadriane.ca@gmail.com>
2017-03-23 19:58:22 +0300

    login worked

--- Backend/db.php
+++ Backend/db.php
@@ -5,4 +5,11 @@ $DB_USER = 'root';
 $DB_PASS = 'UPKFA<72-(';
 $DB_NAME = 't_project_ideas';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
 ?>
---
+++ db/db_config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', "UPKFA<72-("); // db password (mention your db password here)
+define('DB_DATABASE', "t_project_ideas"); // database name
+define('DB_SERVER', "localhost"); // db server
+
+?>
