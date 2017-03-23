223ab3d583e6464eddb4c0525d320885bff6bd10
eric <ericmuchenah@gmail.comcls>
2017-03-23 20:02:24 +0300

    hb

--- Backend/db.php
+++ Backend/db.php
@@ -5,4 +5,10 @@ $DB_USER = 'root';
 $DB_PASS = 'UPKFA<72-(';
 $DB_NAME = 't_project_ideas';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
 ?>
