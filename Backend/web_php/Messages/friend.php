849595f5de849df5c9e4ad82097c9118efa0f6f6
eric <ericmuchenah@gmail.comcls>
2017-03-24 16:39:41 +0300

    Upload attachments

--- Backend/web_php/Messages/friend.php
+++
@@ -1,5 +0,0 @@
-<?php 
-session_start();
-if(isset($_GET['userID'])){
-$_SESSION['friend_to_chat'] = $_GET['userID'];
-}
--- Backend/web_php/getshareid.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-
-if(isset($_POST['shareID'])){
-									
-$_SESSION['commentShare'] = $_POST['shareID'];	
-
-}
-?>
